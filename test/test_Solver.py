cb7b36e30f2179046daa20c0bd5b5b3753d5af5f
zehao99 <zehaoli99@gmail.com>
2021-01-08 16:04:51 +0900

    Changed to smaller mesh for testing.

--- test/test_Solver.py
+++ test/test_Solver.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 from CEIT.Solver import reinitialize_solver
+from CEIT.EJAC import EJAC
 
 class TestSolver(TestCase):
     def test_reinitialize_solver(self):
+        self.EJAC = EJAC()
+        self.EJAC.save_JAC_np()
         reinitialize_solver(203)
+        # pass
